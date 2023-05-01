5170d09fdf4f4c309c1daf5116d097e71df38018
Shivam Attarkar <shivamattarkar89@gmail.com>
2023-05-01 14:07:15 +0530

    daisy ui library added

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,16 @@ module.exports = {
   },
   plugins: [
     require( '@tailwindcss/typography' ),
+    require( "daisyui" )
   ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "--",
+    darkTheme: "dark",
+  }
 }
